ade28d5af23cc35e1115a47dcc3b48d7afd36654
Verica Spaskovska <77109037+verica02@users.noreply.github.com>
2022-09-09 18:04:11 +0200

    Time: 652 ms (22.68%), Space: 90.4 MB (55.67%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.js
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.js
@@ -3,10 +3,21 @@
  * @return {number}
  */
 
-const numberOfWeakCharacters = (properties, count = 0, max_d = 0) => {
-    properties.sort((a,b) => b[0] - a[0] || a[1] - b[1]).forEach(res => {
-        if(res[1] < max_d) count++;
-        if(res[1] > max_d) max_d = res[1];
-    });
+var numberOfWeakCharacters = (properties, count = 0, maxDefence = 0) => {
+    
+    properties.sort((a,b) => b[0] - a[0] || a[1] - b[1])
+    
+              .forEach(res => {
+        
+                if(res[1] < maxDefence){
+                    count++;
+                }
+        
+                if(res[1] > maxDefence){
+                    maxDefence = res[1];
+                }
+        
+             });
+    
     return count;
 };
