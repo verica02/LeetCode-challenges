a37cea47515e99b83108356c7d255e5bf3a7d7f6
Verica Spaskovska <77109037+verica02@users.noreply.github.com>
2022-09-15 17:16:08 +0200

    Time: 10031 ms (6.25%), Space: 66.8 MB (93.75%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} changed
+ * @return {number[]}
+ */
+var findOriginalArray = function(changed) {
+    
+    if(changed.length % 2 !== 0){
+        
+        return []; 
+    }
+    
+    let double = true;
+    
+    changed.sort((a,b) => a-b) 
+    
+    let i = 0;
+    
+    while(i < changed.length){
+        
+        let pos = changed.indexOf(changed[i]*2, i+1); 
+        
+        if(pos === -1){
+            
+            double = false; 
+            
+            break;
+            
+        }else {
+            
+            changed.splice(pos, 1); 
+            
+            i++;
+        } 
+    }
+    
+    return (double? changed: []);
+};
